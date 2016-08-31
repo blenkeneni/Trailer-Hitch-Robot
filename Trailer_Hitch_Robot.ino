eaf70fb4b62f97561170fcddda23a0175577a3b5
blenkeneni <astroblen@yahoo.com>
2016-08-31 12:59:17 -0400

    Update Trailer_Hitch_Robot.ino

--- Trailer_Hitch_Robot.ino
+++ Trailer_Hitch_Robot.ino
@@ -1,4 +1,5 @@
 #include <XBOXRECV.h>
+#include "pitches.h"                    //need to make this header file
 
 const int actrCtrlPin1 = 2;             //Pin to control actuator movement
 const int actrCtrlPin2 = 3;             //Pin to control actuator movement
@@ -11,6 +12,23 @@ int motorDir = 8; //Dir 1
 int motorSpeed = 9; //PWM 1
 int motorDir2 = 10; //Dir 2
 int motorSpeed2 = 11; //PWM2
+int buzzer = 13; //buzzer on pin 13
+
+//these outputs are all for testing purposes 
+int forwardLED = 1; 
+int leftLED = 2;
+int rightLED = 3;
+int upLED = 4; 
+int downLED = 5; 
+int stopLED = 6;
+
+int melody[] = {                         //select the notes you want to play
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4 
+};
+
+int noteDurations[] = {                 //4= quarter note, 8 = either note
+   4, 8, 8, 4, 4, 4, 4, 4
+};
 
 USB Usb;
 XBOXRECV Xbox(&Usb);
@@ -24,6 +42,15 @@ void setup() {
   pinMode(motorDir2, OUTPUT); //Movement Part
   pinMode(motorSpeed, OUTPUT); //Movement Part
   pinMode(motorSpeed2, OUTPUT); //Movement Part
+  
+  pinMode(buzzer, OUTPUT); //buzzer
+  pinMode(forwardLED, OUTPUT);
+  pinMode(leftLED, OUTPUT);
+  pinMode(rightLED, OUTPUT);
+  pinMode(upLED, OUTPUT);
+  pinMode(downLED, OUTPUT);
+  pinMode(stopLED, OUTPUT);
+  
 }
 
 void loop() {
@@ -32,14 +59,14 @@ void loop() {
     int pos = analogRead(hallSensorPin);                  //Get actuator vertical position
     if (Xbox.getAnalogHat(RightHatY , 0) > 1000) {        //Check if Right Stick is in up direction
       if (pos < limitTop) {                               //Check if actuator can extend
-        actuatorUp();
+        actuatorUp();      
       } else {
-        actuatorHold();
+        actuatorHold();     
       }
     } 
     else if(Xbox.getAnalogHat(RightHatY, 0) < 1000) {   //Check if Right Stick is in down direction
       if (pos > limitBot) {                               //Check if actuator can retract
-        actuatorDown();
+        actuatorDown();     
       } 
       else {
         actuatorHold();
@@ -70,11 +97,13 @@ delay(1);
 void actuatorUp() {
   digitalWrite(actrCtrlPin1, HIGH);
   digitalWrite(actrCtrlPin2, LOW);
+  digitalWrite(upLED, HIGH);
 }
 
 void actuatorDown() {
   digitalWrite(actrCtrlPin1, LOW);
   digitalWrite(actrCtrlPin2, HIGH);
+  digitalWrite(downLED, HIGH);
 }
 
 void actuatorHold() {
@@ -82,6 +111,18 @@ void actuatorHold() {
   digitalWrite(actrCtrlPin2, LOW);
 }
 
+for (int thisNote = 0; thisNote < 8; thisNote++) {
 
+    //use this to calculate noteDuration: quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(13);
+  }
 
 //Stephanie Stuff for movement
